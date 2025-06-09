812e72124cc5f7c37c6bdd76a10d2a991c85dc42
jeyjey25 <jeanmatiaskuhn@alunos.utfpr.edu.br>
2025-06-09 13:05:05 -0300

    Delete AP06Repository directory

--- AP06Repository/Interfaces/IProdutoRepository.cs
+++
@@ -1,8 +0,0 @@
-public interface IProdutoRepository
-{
-    void Adicionar(Produto produto);
-    Produto? ObterPorId(Guid id);
-    List<Produto> ObterTodos();
-    void Atualizar(Produto produto);
-    bool Remover(Guid id);
-}
